e6dda546c0b7025c51f2175dc8b1d4a39c8d09f5
junn999 <junioralberto1421@gmail.com>
2022-07-31 18:28:30 -0600

    Agregar archivos de proyecto.

---
+++ cuarto_proyecto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cuarto_proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ tercer_programa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tercer_programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
